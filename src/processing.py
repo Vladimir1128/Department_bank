bc17e32b8f5b5096e3105b2842140b6ffc76d516
Vladimir1128 <vkorobkin1128@gmail.com>
2025-04-24 19:00:56 +0700

    Создание модуля processing.py и функции filter_by_state.

---
+++ src/processing.py
@@ -0,0 +1,24 @@
+def filter_by_state(list_dictionaries: list, state: str) -> list:
+    """
+    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state соответствует указанному значению.
+    """
+    list_dictionaries_1 = []
+    for key in list_dictionaries:
+        if state == key["state"]:
+            list_dictionaries_1.append(key)
+        elif state == "" and key["state"] == "EXECUTED":
+            list_dictionaries_1.append(key)
+    return list_dictionaries_1
+
+
+if __name__ == "__main__":
+    state = input("Введите состояние: ")
+    list_dictionaries = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+    result = filter_by_state(list_dictionaries, state)
+    print(result)
