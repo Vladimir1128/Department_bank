04afd833325a4057451dc18d04198ebf5f211bfa
Vladimir1128 <vkorobkin1128@gmail.com>
2025-06-12 18:11:14 +0700

    Создание пакета data и модуля utils.py

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import json
+from typing import Any
+
+
+def way_to_json(json_file: str) -> list[Any]:
+    """
+    Функция принимает на вход путь до JSON-файла
+    и возвращает список словарей с данными о финансовых транзакциях
+    """
+    try:
+        with open(json_file, encoding="utf-8") as file_in:
+            data = json.load(file_in)
+
+        if isinstance(data, list):
+            return data
+        else:
+            return []
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
+
+
+if __name__ == "__main__":
+    print(way_to_json("data/operations.json"))
