3a57413b19045e59124799ba630f6d2dadc70183
Vladimir1128 <vkorobkin1128@gmail.com>
2025-07-03 21:41:57 +0700

    Создал лог в модуле masks.py для get_mask_account

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,14 @@
 import logging
 
-mask_card_number_logger = logging.getLogger('app.masks')
-file_handler = logging.FileHandler('../logs/masks.log')
+mask_card_number_logger = logging.getLogger('app.mask_number')
+mask_account_logger = logging.getLogger('app.mask_account')
+file_handler = logging.FileHandler('logs/masks.log')
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(filename)s - %(funcName)s - %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 mask_card_number_logger.addHandler(file_handler)
 mask_card_number_logger.setLevel(logging.DEBUG)
-
+mask_account_logger.addHandler(file_handler)
+mask_account_logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(number: int | str | None) -> str | None:
@@ -18,18 +20,27 @@ def get_mask_card_number(number: int | str | None) -> str | None:
             mask_card_number_logger.info(f'Program works correctly!')
             return mask_number
         else:
-            mask_card_number_logger.info(f'Unsuccessful  program termination!')
+            mask_card_number_logger.warning(f'Unsuccessful  program termination!')
     except Exception as e:
         mask_card_number_logger.error(f'An error occurred!: {e}', exc_info=True)
     finally:
         mask_card_number_logger.info(f'Completion of work!')
 
+
 def get_mask_account(account: int | str | None) -> str | None:
     """Функция принимает на вход номер счета и возвращает его маску. """
-    account_str = str(account)
-    if len(account_str) == 20 and len(account_str) > 0 and account_str.isdigit():
-        mask_account = ("*" * 2) + account_str[-4:]
-        return mask_account
+    try:
+        account_str = str(account)
+        if len(account_str) == 20 and len(account_str) > 0 and account_str.isdigit():
+            mask_account = ("*" * 2) + account_str[-4:]
+            mask_account_logger.info(f'Program works correctly!')
+            return mask_account
+        else:
+            mask_account_logger.warning(f'Unsuccessful  program termination!')
+    except Exception as e:
+        mask_account_logger.error(f'An error occurred!: {e}', exc_info=True)
+    finally:
+        mask_account_logger.info(f'Completion of work!')
 
 
 if __name__ == "__main__":
