9f66ae8f3074f2ef34a35db324c94747ff5d8f14
Vladimir1128 <vkorobkin1128@gmail.com>
2025-07-03 22:31:29 +0700

    My homework12_2

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,9 @@
 import logging
 
-mask_card_number_logger = logging.getLogger('app.mask_number')
-mask_account_logger = logging.getLogger('app.mask_account')
-file_handler = logging.FileHandler('logs/masks.log')
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(filename)s - %(funcName)s - %(levelname)s: %(message)s')
+mask_card_number_logger = logging.getLogger("app.mask_number")
+mask_account_logger = logging.getLogger("app.mask_account")
+file_handler = logging.FileHandler("logs/masks.log")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(filename)s - %(funcName)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 mask_card_number_logger.addHandler(file_handler)
 mask_card_number_logger.setLevel(logging.DEBUG)
@@ -13,34 +13,36 @@ mask_account_logger.setLevel(logging.DEBUG)
 
 def get_mask_card_number(number: int | str | None) -> str | None:
     """Функция принимает на вход номер карты и возвращает ее маску."""
+    mask_card_number_logger.info("Starts work!")
     try:
         number_str = str(number)
         if len(number_str) == 16 and len(number_str) > 0 and number_str.isdigit():
             mask_number = number_str[:4] + " " + number_str[4:6] + ("*" * 2 + " " + "*" * 4) + " " + number_str[12:]
-            mask_card_number_logger.info(f'Program works correctly!')
+            mask_card_number_logger.info("Program works correctly!")
             return mask_number
         else:
-            mask_card_number_logger.warning(f'Unsuccessful  program termination!')
+            mask_card_number_logger.warning("Unsuccessful  program termination!")
     except Exception as e:
-        mask_card_number_logger.error(f'An error occurred!: {e}', exc_info=True)
+        mask_card_number_logger.error(f"An error occurred!: {e}", exc_info=True)
     finally:
-        mask_card_number_logger.info(f'Completion of work!')
+        mask_card_number_logger.info("Completion of work!")
 
 
 def get_mask_account(account: int | str | None) -> str | None:
-    """Функция принимает на вход номер счета и возвращает его маску. """
+    """Функция принимает на вход номер счета и возвращает его маску."""
+    mask_account_logger.info("Starts work!")
     try:
         account_str = str(account)
         if len(account_str) == 20 and len(account_str) > 0 and account_str.isdigit():
             mask_account = ("*" * 2) + account_str[-4:]
-            mask_account_logger.info(f'Program works correctly!')
+            mask_account_logger.info("Program works correctly!")
             return mask_account
         else:
-            mask_account_logger.warning(f'Unsuccessful  program termination!')
+            mask_account_logger.warning("Unsuccessful  program termination!")
     except Exception as e:
-        mask_account_logger.error(f'An error occurred!: {e}', exc_info=True)
+        mask_account_logger.error(f"An error occurred!: {e}", exc_info=True)
     finally:
-        mask_account_logger.info(f'Completion of work!')
+        mask_account_logger.info("Completion of work!")
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
 import json
+import logging
 from typing import Any
+
+way_to_json_logger = logging.getLogger("app.way_json")
+file_handler = logging.FileHandler("logs/utils.log")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(filename)s - %(funcName)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+way_to_json_logger.addHandler(file_handler)
+way_to_json_logger.setLevel(logging.DEBUG)
 
 
 def way_to_json(json_file: str) -> list[Any]:
@@ -7,21 +15,27 @@ def way_to_json(json_file: str) -> list[Any]:
     Функция принимает на вход путь до JSON-файла
     и возвращает список словарей с данными о финансовых транзакциях
     """
+    way_to_json_logger.info("Starts work!")
     try:
         with open(json_file, encoding="utf-8") as file_in:
             data = json.load(file_in)
 
         if isinstance(data, list):
+            way_to_json_logger.info("Program works correctly!")
             return data
         else:
+            way_to_json_logger.warning("Unsuccessful  program termination!")
             return []
-    except FileNotFoundError:
+    except FileNotFoundError as e:
+        way_to_json_logger.error(f"An error occurred!: {e}")
         return []
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
+        way_to_json_logger.error(f"An error occurred!: {e}")
         return []
+    finally:
+        way_to_json_logger.info("Completion of work!")
 
 
 if __name__ == "__main__":
     transactions_ = way_to_json("data/operations.json")
     print(transactions_)
-
